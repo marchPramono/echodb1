547b7af1d930dff6f02fe2ec6a45e0fd49975ea9
marchPramono <march.pramono@gmail.com>
2019-03-12 23:04:27 +0700

    edit db status

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ var (
 	err error
 )
 
+// Employee data
 type Employee struct {
 	ID     string `json:"id"`
 	Name   string `json:"name"`
@@ -23,8 +24,43 @@ type Employee struct {
 	Email  string `json:"email"`
 }
 
+// Employees object
 type Employees struct {
 	Employees []Employee `json:"employees"`
+}
+
+func init() {
+
+	connStr := "host=localhost port=5432 user=postgres password=FR4uT dbname=employee_db sslmode=disable"
+	db, err = sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		panic(err)
+	} else {
+		fmt.Println("DB Connected...")
+	}
+}
+
+func main() {
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Server connected!")
+	})
+
+	e.POST("/employee", addEmployee)
+	e.PUT("/employee/:id", updateEmployee)
+	e.DELETE("/employee/:id", deleteEmployee)
+	e.GET("/employee/:id", getEmployee)
+	e.GET("/employee", getAllEmployee)
+
+	e.Logger.Fatal(e.Start(":1323"))
+
 }
 
 func addEmployee(c echo.Context) error {
@@ -41,48 +77,6 @@ func addEmployee(c echo.Context) error {
 		return c.JSON(http.StatusCreated, u)
 	}
 	return c.String(http.StatusOK, "ok")
-}
-
-func updateEmployee(c echo.Context) error {
-	u := new(Employee)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	sqlStatement := `UPDATE employees SET name=$1, salary=$2, age=$3, email=$4 WHERE id=$5`
-	res, err := db.Query(sqlStatement, u.Name, u.Salary, u.Age, u.Email, u.ID)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(res)
-		return c.JSON(http.StatusCreated, u)
-	}
-	return c.String(http.StatusOK, u.ID)
-}
-
-func deleteEmployee(c echo.Context) error {
-	id := c.Param("id")
-	sqlStatement := `DELETE FROM employees WHERE id=$1`
-	res, err := db.Query(sqlStatement, id)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(res)
-		return c.JSON(http.StatusOK, "Deleted")
-	}
-	return c.String(http.StatusOK, id+"Deleted")
-}
-
-func getEmployee(c echo.Context) error {
-	id := c.Param("id")
-	sqlStatement := `SELECT FROM employees WHERE id=$1`
-	res, err := db.Query(sqlStatement, id)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(res)
-		return c.JSON(http.StatusOK, "Selected")
-	}
-	return c.String(http.StatusOK, id+"Selected")
 }
 
 func getAllEmployee(c echo.Context) error {
@@ -105,33 +99,44 @@ func getAllEmployee(c echo.Context) error {
 	return c.JSON(http.StatusCreated, result)
 }
 
-func main() {
-
-	connStr := "host=localhost port=5432 user=postgres password=FR4uT dbname=employee_db sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-
+func updateEmployee(c echo.Context) error {
+	u := new(Employee)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	sqlStatement := `UPDATE employees SET name=$1, salary=$2, age=$3, email=$4 WHERE id=$5`
+	res, err := db.Query(sqlStatement, u.Name, u.Salary, u.Age, u.Email, u.ID)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := db.Ping(); err != nil {
-		panic(err)
+		fmt.Println(err)
 	} else {
-		fmt.Println("DB Connected...")
+		fmt.Println(res)
+		return c.JSON(http.StatusCreated, u)
 	}
+	return c.String(http.StatusOK, u.ID)
+}
 
-	e := echo.New()
+func getEmployee(c echo.Context) error {
+	id := c.Param("id")
+	sqlStatement := `SELECT FROM employees WHERE id=$1`
+	res, err := db.Query(sqlStatement, id)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+		return c.JSON(http.StatusOK, "Selected")
+	}
+	return c.String(http.StatusOK, id+"Selected")
+}
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Server connected!")
-	})
-
-	e.POST("/employee", addEmployee)
-	e.PUT("/employee", updateEmployee)
-	e.DELETE("/employee/:id", deleteEmployee)
-	e.GET("/employee/:id", getEmployee)
-	e.GET("/employee", getAllEmployee)
-
-	e.Logger.Fatal(e.Start(":1323"))
-
+func deleteEmployee(c echo.Context) error {
+	id := c.Param("id")
+	sqlStatement := `DELETE FROM employees WHERE id=$1`
+	res, err := db.Query(sqlStatement, id)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+		return c.JSON(http.StatusOK, "Deleted")
+	}
+	return c.String(http.StatusOK, id+"Deleted")
 }
